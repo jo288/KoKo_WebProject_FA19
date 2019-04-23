7ee7805a6003d4a1bd0ae0241843c47bd98e4c1a
Jinju Ouck <jo288@cornell.edu>
2019-04-22 23:23:09 -0400

    updated footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,18 @@
 <footer>
-    <!-- Sample Footer -->
-    <p id="footer_copyright">
-        Copyright: KoKo & Golden Beaver Team<br>
-        Background Image Copyright: Welsley, <a herf="https://www.toptal.com/designers/subtlepatterns/inspiration-geometry/">Subtle Patterns</a>
-    </p>
+    <div id="copyright_container">
+        <h2> Copyright </h2>
+        <p id="footer_copyright">
+            Design By: KoKo & Golden Beaver Team<br>
+            Background Image: Welsley, <a href="https://www.toptal.com/designers/subtlepatterns/inspiration-geometry/">Subtle Patterns</a>
+        </p>
+    </div>
+    <div id="contact_container">
+        <h2> Contact </h2>
+        <p>
+            321 College Ave, Ithaca, NY 14850
+        </p>
+        <p>
+            (607) 277-8899
+        </p>
+    </div>
 </footer>
