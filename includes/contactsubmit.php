75ecdbe16cb5c2ffd91f6dc7512a74b2201eeb19
Khalid Nadi <kn395@cornell.edu>
2019-05-07 11:46:37 -0400

    contact page validation, contact submit delete works, css reformating

--- includes/contactsubmit.php
+++ includes/contactsubmit.php
@@ -1,42 +1,61 @@
     <!-- Show Contact Submissions -->
     <fieldset class='contactsubmiss'>
         <?php
-
-
-
         $sql = "SELECT * FROM contact";
         $params = array();
         $result = exec_sql_query($db, $sql, $params);
         $records = $result->fetchAll();
         foreach ($records as $record) {
-            $deleterkey = '\'deleter' .' '. $record['id'].'\'';
-            echo ($deleterkey);
+            $deleterkey = '\'deleter' . ' ' . $record['id'] . '\'';
             if (isset($_GET[$deleterkey])) {
                 echo ("Contact issue successfully deleted from entries.");
-                // $arr = explode(".", $record['id'], 2);
-                // $sql = "DELETE FROM contact WHERE id =  $arr[0];";
-                // exec_sql_query($db, $sql);
-                // unlink(GALLERY_UPLOADS_PATH . $_GET['id']);
+            }
 
+            if (isset($_GET["deleter"])) {
+                //DELETE CCONTACT RESPONSE
+                $sql = "SELECT * FROM contact WHERE id = :id;";
+                $params = array(
+                    ':id' => $_GET["deleter"]
+                );
+                $records = exec_sql_query($db, $sql, $params)->fetchAll();
+                if ($records) {
+                    foreach ($records as $record) {
+                        $sql = "DELETE FROM contact WHERE id = :id;";
+                        $params = array(
+                            ':id' => $record["id"]
+                        );
+                        $records = exec_sql_query($db, $sql, $params);
+                        if ($records) {
+                            header("Refresh:0");
+                         } else {
+                            echo "<h2 class='delete_alert'> Failed to delete Contact Submission </h2>";
+                        }
+                    }
+                }
             }
 
             ?>
+
+
+
+
+
             <div class="display_review">
                 <!--Each table column is echoed into a td cell-->
-                <div id="left" class="inner_display_review">
-                    <p><?php echo htmlspecialchars($record['name']); ?></p>
+                <div class="inner_display_contactl">
+                    <p><strong><?php echo htmlspecialchars($record['name']); ?></strong></p>
                     <p><?php echo htmlspecialchars($record['email']); ?></p>
                     <p><?php echo htmlspecialchars($record['reason']); ?></p>
                     <p><?php echo htmlspecialchars($record['delivery']); ?></p>
                 </div>
-                <div id="right" class="inner_display_review">
+                <div class="inner_display_contactr">
                     <p>
-                        <p id="display_review_title"><?php echo htmlspecialchars($record['review_title']); ?></p>
+                        <p><?php echo htmlspecialchars($record['review_title']); ?></p>
                         <p><?php echo htmlspecialchars($record['text']); ?></p>
                 </div>
                 <div>
-                    <form id="deleterform" action="contact.php" method="get">
-                        <button name="deleter <?php echo ($record['id']) ?>" id='deleter' type="submit">Delete</button>
+                    <form action="employee.php" method="get">
+                        <button name="deleter" class='deleter2' value="<?php echo ($record["id"]); ?>" type="submit">Delete</button>
                         <?php
 
                         ?>
